a3ad1935dec548eb3d5e9cab0d56204d96473ad7
Cristian Montes <cmontesmujica@gmail.com>
2021-08-17 16:38:45 -0700

    added cows details inputs and displaying

--- src/Cows/CowsDetail.js
+++ src/Cows/CowsDetail.js
@@ -1,10 +1,80 @@
 import React, { Component } from 'react';
+import { getACow, getBreeds } from '../utils/fetch-utils';
 
 class CowsDetails extends Component {
-    state = {  }
+    state = { 
+        id:0,
+        sex:'',
+        number_horns:0,
+        milk: '',
+        breeds:[]
+     };
+
+
+     componentDidMount = async () => {
+         const cowsId = this.props.match.params.id;
+        //  console.log(cowsId);
+         const aCowData = await getACow(cowsId);
+        //  console.log([aCowData]);
+         const breeds = await getBreeds();
+         
+         this.setState({...aCowData, breeds});
+     }
+
+
     render() { 
         return ( 
-            <h1>WELCOME TO THE DETAILS PAGE</h1>
+            <>
+                <p>Cow's Name is Numero-{this.state.id}</p>
+                <form id='update-cow'>
+                     
+                    <div className='form-cow'>
+                        <label>Sex:</label>
+                        <input
+                            onChange={(event) => {
+                                this.setState({sex: event.target.value})
+                            }}
+                            type="text"
+                            value={this.state.sex}
+                        ></input>
+                    </div>
+
+                    <div className='form-cow'>
+                        <label>Number of Horns:</label>
+                        <input
+                            onChange={(event) => {
+                                this.setState({number_horns: event.target.value})
+                            }}
+                            type="text"
+                            value={this.state.number_horns}
+                        ></input>
+                    </div>
+
+                    <div className='form-cow'>
+                        <label>Chocolate Milk:</label>
+                        <input
+                            onChange={(event) => {
+                                this.setState({milk: event.target.value})
+                            }}
+                            type="text"
+                            value={this.state.milk}
+                        ></input>
+                    </div>
+
+                    <div className='form-cow'>
+                        <label>Breed:</label>
+                        <select>
+                            {this.state.breeds.map((bred) => {
+                                return(
+                                    <option value={bred.breed_type}>{bred.breed_type}</option>
+                                );
+                            })}
+                        </select>
+                    </div>
+
+                </form>
+
+            </>
          );
     }
 }
--- src/utils/fetch-utils.js
+++ src/utils/fetch-utils.js
@@ -1,7 +1,22 @@
 const URL = 'https://quiet-lowlands-69499.herokuapp.com'  //benchmark url for fetching data
 
+// FUNCTION TO GET COW'S DATA FROM API
 export const getCows = async ()=>{
     const resp = await fetch(`${URL}/cows`);
+    const data = await resp.json();
+    return data;
+}
+
+// FUNCTION TO GET A COW'S DATA FROM API BY ID
+export const getACow = async (id)=>{
+    const resp = await fetch(`${URL}/cows/${id}`);
+    const data = await resp.json();
+    return data;
+}
+
+// FUNCTION TO GET BREEDS DATA FROM API
+export const getBreeds = async ()=>{
+    const resp = await fetch(`${URL}/breeds`);
     const data = await resp.json();
     return data;
 }
